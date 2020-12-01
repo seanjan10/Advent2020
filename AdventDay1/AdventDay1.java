9892309d37cd850393dcb81f0c15660e68f6f5b3
Sean Janssens <49081307+seanjan10@users.noreply.github.com>
2020-12-01 01:40:40 -0500

    Create AdventDay1.java

---
+++ AdventDay1/AdventDay1.java
@@ -0,0 +1,31 @@
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Scanner;
+import java.io.*;
+public class AdventDay1 {
+
+
+    public static void main(String args[]) throws IOException{
+        File input = new File("input.txt");
+        Scanner fr = new Scanner(new FileInputStream(input));
+        HashSet<Integer> set = new HashSet<Integer>();
+        while(fr.hasNextLine()) {
+            set.add(fr.nextInt());
+        }
+        int year = 2020;
+        int numberToMult1 = 0;
+        int numberToMult2 = 0;
+        Iterator<Integer> iter = set.iterator();
+        while (iter.hasNext()) {
+            int temp = iter.next();
+            int valNeeded = 2020 - temp;
+            if (set.contains(valNeeded)) {
+                numberToMult1 = valNeeded;
+                numberToMult2 = temp;
+                break;
+            }
+        }
+        System.out.println(numberToMult1 * numberToMult2);
+    }
+}
