fab3e39ea8ff1928d511703220d66117d496644c
Sean Janssens <49081307+seanjan10@users.noreply.github.com>
2020-12-02 20:02:31 -0500

    forgot to upload part 2

--- AdventDay1/AdventDay1.java
+++ AdventDay1/AdventDay1.java
@@ -15,16 +15,33 @@ public class AdventDay1 {
         int year = 2020;
         int numberToMult1 = 0;
         int numberToMult2 = 0;
+        int numberToMult3 = 0;
+
         Iterator<Integer> iter = set.iterator();
-        while (iter.hasNext()) {
+        
+       /* while (iter.hasNext()) {
             int temp = iter.next();
             int valNeeded = 2020 - temp;
             if (set.contains(valNeeded)) {
                 numberToMult1 = valNeeded;
                 numberToMult2 = temp;
                 break;
-            }
-        }
-        System.out.println(numberToMult1 * numberToMult2);
+            } */
+            while (iter.hasNext()) {
+                int temp = iter.next();
+                int valNeeded = 2020 - temp;
+                Iterator<Integer> iter2 = set.iterator();
+                while (iter2.hasNext()) {
+                    int temp2 = iter2.next();
+                    int valNeeded2 = valNeeded - temp2;
+                    if (set.contains(valNeeded2)) {
+                        numberToMult1 = temp;
+                        numberToMult2 = temp2;
+                        numberToMult3 = valNeeded2;
+                        break;
+                    }
+                }
+        } 
+        System.out.println(numberToMult1 * numberToMult2 * numberToMult3);
     }
 }
